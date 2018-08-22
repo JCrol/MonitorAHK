30caf85c9c5b369490d9cc90c584a7dfde6640a5
Daniel Medrano <bestenglishking@hotmail.com>
2018-08-22 18:10:32 -0500

    Correcciones

--- Monitor AHK/Form1.cs
+++ Monitor AHK/Form1.cs
@@ -75,10 +75,26 @@ namespace Monitor
         private void ActualizarVistas()
         {
             // Backlog
-            DataSet dataSet = Sql.Ejecutar("DECLARE @FechaLocal DATETIME DECLARE @FechaGTM DATETIME DECLARE @FechaInicio DATETIME SET @FechaLocal = (SELECT GETDATE()) SET @FechaGTM = (SELECT DATEADD(hour, 6, @FechaLocal)) SET @FechaInicio = (SELECT CONVERT(char(10), @FechaGTM, 112)) SELECT iLoggerID AS 'LOGGER', SUM(ISNULL(CAST(iCount AS BIGINT), 0)) AS 'PENDIENTES POR ARCHIVAR' FROM nice_storage_center.dbo.vwScBacklog WHERE dtRecordingGMTStartTime BETWEEN @FechaInicio AND @FechaGTM GROUP BY iLoggerID");
+            DataSet dataSet;
 
-            if (dataSet.Tables.Count > 0)
-                dataGridBacklog.DataSource = dataSet.Tables[0];
+            try
+            {
+                dataSet = Sql.ObtenerDataSet("DECLARE @FechaLocal DATETIME DECLARE @FechaGTM DATETIME DECLARE @FechaInicio DATETIME SET @FechaLocal = (SELECT GETDATE()) SET @FechaGTM = (SELECT DATEADD(hour, 6, @FechaLocal)) SET @FechaInicio = (SELECT CONVERT(char(10), @FechaGTM, 112)) SELECT iLoggerID AS 'LOGGER', SUM(ISNULL(CAST(iCount AS BIGINT), 0)) AS 'PENDIENTES POR ARCHIVAR' FROM nice_storage_center.dbo.vwScBacklog WHERE dtRecordingGMTStartTime BETWEEN @FechaInicio AND @FechaGTM GROUP BY iLoggerID",
+                    "Data Source=DBEFIDIA; Initial Catalog=nice_storage_center; Persist Security Info=True; User ID=nicevb; Password=N1c3ct134");
+
+                if (dataSet.Tables.Count > 0)
+                    dataGridBacklog.DataSource = dataSet.Tables[0];
+            }
+            catch (System.Data.SqlClient.SqlException e)
+            {
+                MessageBox.Show("Ha ocurrido un error relacionado con la base de datos o la instancia SQL." +
+                    Environment.NewLine + e.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Ha ocurrido un error desconocido." + Environment.NewLine +
+                    e.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
